fa9e83bc71ca93e321ece6e441cd297f5ef31b7d
Sam Goldstein <sam@stackery.io>
2018-09-15 10:47:30 -0700

    Add lambda function code to build frontend

--- src/deployFrontEnd/index.js
+++ src/deployFrontEnd/index.js
@@ -1,5 +1,9 @@
+const { spawnSync } = require('child_process');
+
 exports.handler = async message => {
   console.log(message);
+  const ret = spawnSync('npm', ['run', 'build'], {'cwd': 'front-end'});
+  console.log(ret);
 
   return {};
-}
+};
