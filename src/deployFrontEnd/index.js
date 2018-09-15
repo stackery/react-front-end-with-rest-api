043b13d3b26a3d8cd0322f8e900e10e5fe666a0e
Sam Goldstein <sam@stackery.io>
2018-09-15 11:12:29 -0700

    Added code to upload front-end/build/index.html to s3

--- src/deployFrontEnd/index.js
+++ src/deployFrontEnd/index.js
@@ -1,9 +1,24 @@
-const { spawnSync } = require('child_process');
+const { spawn } = require('child_process');
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+
+const AWS = require('aws-sdk');
+const s3 = AWS.s3;
 
 exports.handler = async message => {
   console.log(message);
-  const ret = spawnSync('npm', ['run', 'build'], {'cwd': 'front-end'});
+  const ret = await spawn('npm', ['run', 'build'], {'cwd': 'front-end'});
   console.log(ret);
+
+  var params = {
+    Body: await readFile('./front-end/build/index.html'),
+    Bucket: process.env.BUCKET_NAME,
+    Key: 'index.html'
+  };
+  console.log(params);
+  const s3Response = await s3.putObject(params).promise();
+  console.log(s3Response);
 
   return {};
 };
